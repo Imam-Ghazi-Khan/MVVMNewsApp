2da2a1d913324ef7a3ce2070cad25b7b20c636ca
igk1710 <igkhan1710@gmail.com>
2023-08-02 00:26:11 +0530

    commit

--- app/src/main/java/com/igk/mvvmnewsapp/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.androiddevs.mvvmnewsapp.utils
-
-class Constants {
-    companion object{
-         const val API_KEY = "b1a1cfa0f55e41d88e1edf80967918fd"
-         const val BASE_URL = "https://newsapi.org/v2/"
-         const val SEARCH_NEWS_TIME_DELAY = 500L
-    }
-}
